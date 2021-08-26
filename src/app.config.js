c819b1b0b9a1527f13b74391bd04347fa1eb52de
venkat10raman <10venkataramana@gmail.com>
2021-08-26 05:59:53 +0000

    Initial commit

--- src/app.config.js
+++ src/app.config.js
@@ -2,6 +2,7 @@
  * Place to store data specific to this instance
  * DO NOT store secrets here
  * DO NOT store state here - must be part of a stateless service
+ * sample change
  */
 class AppConfig {
     version = '1.0.1';
